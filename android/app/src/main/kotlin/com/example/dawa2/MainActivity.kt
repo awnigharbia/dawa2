e97f0329b9f49978a6b0c54073a92c2420fd1fc4
Awni Gharbia <awni2009@hotmail.com>
2021-03-11 19:32:04 +0200

    fix flutter run --release

--- android/app/src/main/kotlin/com/example/dawa2/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dawa2/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.dawa2
 
-import io.flutter.embedding.android.FlutterActivity
+import android.util.Log
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.util.GeneratedPluginRegister
+import java.lang.reflect.Method
 
-class MainActivity: FlutterActivity() {
+
+class EmbeddingV2Activity : FlutterFragmentActivity() {
+
 }
